19493a85111a27a82413ac113cbcf87f7abcef11
joannasummers <jojo.summers@hotmail.com>
2015-06-05 18:27:24 +0200

    Fixed the use of the encrypt/decrypt functions
    Imported the encrypt and decrypt functions, set it to take the data
    input in the form and to generate a result using these functions.
    Result is posted on the encrypt/decrypt result page.
    Fixed all the html pages to work with this.

--- crypter.py
+++ crypter.py
@@ -1,84 +1,74 @@
-# write the cell into a named python file rather than ipynb, may be used with flaskr
+#write all the code from this cell into a python file, of a set name and directory.
+#running this file in the command line will launch the application.
 
-# import the required modules from flask
+#import the required modules from flask
 from flask import Flask, request, render_template, url_for, flash, session, redirect
-from werkzeug import secure_filename
-import os
+import jinja2
 
-#import the form module created
+#import the form module created in the crypter_forms.py file.
 from crypter_forms import EncryptionForm, DecryptionForm
-from crypter_config import init_db
+
+#import the modules with encrypt and decrypt functions in the encrypt.py and decrypt.py file.
+from encrypt import encrypt
+from decrypt import decrypt
 
 #use the flask module to create an application
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-#UPLOAD_FOLDER = '/C:/flaskr/uploads'
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app route sets the route for each page of the website.
+#each page renders the corresponding html file, all extend from template.html to give a uniform website.
+#give each page a unique title to display on the website.
 
-# set the route for each page.
-# show the template rendered with the specific page.
+#home page of DNA-Crypter 
 @app.route('/')
 def run_homepage():
     return render_template('homepage.html', title="Home")
 
-#@app.route('/encrypt_result', methods=('GET', 'POST'))
-#def run_encrypt_result():
-#    return render_template('encrypt_result.html', title="Encryption Result")
-
-message_input = ""
-
+#encrypt page, uses the EncryptionForm imported from the other module.
+#returns the encrypt_result render if the HTTP method is POST (form is being submitted), and the form is valid with no errors.
+#encrypt_result extends from encrypt template, remaining on the same page.
+#takes the data from the form, adds to variables for message, key, level and DNA_sequence.
+#this data is used in the encrypt function to give a result, the encrypted DNA sequence, which is passed onto the encrypt_result template.
+#else returns the encrypt template render, keeping the same page if the HTTP method is GET or thr form is invalid.
 @app.route('/encrypt', methods=('GET', 'POST'))
 def run_encrypt():
     form = EncryptionForm(request.form)
-    global message_input
-    message_input = ""
-    #if form.validate_on_submit():
     if request.method == 'POST' and form.validate():
-        #dict = {}
-        #dict['key_input'] = 'form.key.data'
-        #key_input.save('/C:/flaskr/uploads/key_save.txt')
-        message_input = "form.message.data"
-        #level_input = 'form.level.data'
-        #test = ''
-        #global test 
-        #test = 'testing'
-        #filename = secure_filename(form.upload_genome.file.filename)
-        #fieldname = field.file.filename.lower()
-        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #form.upload_genome.data.save('home/student/flaskr/uploads/' + filename)
-        return render_template('encrypt_result.html', title="Encrypt", form=form)
-        #return url_for('run_encrypt_result')
+        message_input = form.message.data
+        key_input = form.key
+        level_input = form.level
+        DNA_sequence_input = form.DNA_sequence.data
+        return render_template('encrypt_result.html', title="Encrypt", form=form, result = encrypt(message_input, key_input, level_input, DNA_sequence_input))  
     else:
         return render_template('encrypt.html', title="Encrypt", form=form)
 
-
+#decrypt page, same theory as the encrypt page above.
+#data gives a result, the decrypted message, which is passed to the decrypt_result template.
 @app.route('/decrypt', methods=('GET', 'POST'))
 def run_decrypt():
     form = DecryptionForm(request.form)
     if request.method == 'POST' and form.validate():
-        enter_key = 'form.enter_key.data'
-        encrypted_DNA = 'form.encrypted_DNA.data'
-        return render_template('decrypt_result.html', title="Decrypt", form=form)
+        enter_key_input = form.enter_key
+        encrypted_DNA_input = form.encrypted_DNA
+        return render_template('decrypt_result.html', title="Decrypt", form=form, result = decrypt(enter_key_input, encrypted_DNA_input)
     else:
         return render_template('decrypt.html', title="Decrypt", form=form)
 
-#@app.route('/decrypt_result', methods=('GET', 'POST'))
-#def run_decrypt_result():
-#    return render_template('decrypt_result.html', title="Decryption Result")
-
+#help page
 @app.route('/help')
 def run_help():
     return render_template('help.html', title="Help")
 
+#about page
 @app.route('/about')
 def run_about():
     return render_template('about.html', title="About")
 
-#set the secret key for the app
+#set the secret key for the application
 app.secret_key = '\x90,\xd7\n\xf5\xe0\r,\x1a\x18\xda\xd40\x11\xe9Q\xe3\xbc\xc6\xf7'
 
-#run the local server with the application
+#runs the local server (http://127.0.0.1:5000) with the application
 #de-bug enabled
 if __name__ == '__main__':
     app.run(debug=True)
--- crypter_forms.py
+++ crypter_forms.py
@@ -1,16 +1,18 @@
-
-#import module to make forms
+#write all the code from this cell into a python file, of a set name and directory.
+    
+#import the flask module, and wtforms to make forms
+#import the various Fields and validators from wtforms, required for the form
 from flask import Flask
-from werkzeug import secure_filename
 from wtforms import Form, PasswordField, StringField, BooleanField, SelectField, TextAreaField, validators
 from wtforms.validators import ValidationError, InputRequired, EqualTo, Length
+
+#did not get FileField working to upload a DNA sequence file, instead just used TextAreaField - kept code if you wanted to see
 #from flask_wtf import Form
-#from flask_wtf.file import FileField, FileAllowed, FileRequired
+#from flask_wtf.file import FileField, FileAllowed, FileRequired 
+#from werkzeug import secure_filename
 
-def key_check(form,field):
-    if len(field.data) < 6:
-        raise ValidationError('Key must be at least 6 characters.')      
-
+#checks if the file uploaded with TextAreaField is the correct format, did not use.
+#the wtform and flask-wtf were interfering with each other preventing the form from being validated.
 #def file_check(form,field):
     #filename = secure_filename(field.data.filename.lower())
     #ALLOWED_EXTENSIONS = set(['txt','fna'])
@@ -19,24 +21,33 @@ def key_check(form,field):
             #raise ValidationError('Wrong file-type, must be fna or txt.')
     #else:
        # raise ValidationError('Please open the DNA sequence.')
-        
-#form for encryption
+    
+#function to check that the key inputted from the form is less than 6 characters, if so gives validation error.
+#no error given if the key is greater than 5 characters - sufficiently long to maintain security of the decryption.
+def key_check(form,field):
+    if len(field.data) < 6:
+        raise ValidationError('Key must be at least 6 characters.')    
+    
+#form for encryption using wtform functions.
+#takes the information of the message and DNA sequence with a Text area box.
+#takes the information of the key with a password box, does not show characters while typing to keep security of key.
+#key is input twice and checks that they are the same with EqualTo, if not gives error.
+#takes the information of level of encryption with a Select field, giving 4 choices of level.
+#InputRequired ensures that section of the form is filled, if not will give error.
 class EncryptionForm(Form):
     message = TextAreaField('Message to be encrypted:', [InputRequired()])
     key = PasswordField('Choose your secret key:', [EqualTo('confirm_key', message='The keys must match.')])
     confirm_key = PasswordField('Re-enter key:', [InputRequired(), key_check])
-            #Length(min=6, message='Key must be at least 6 characters.')])
     level = SelectField('Choose the level of encryption:', choices=[('1','Level 1'), ('2','Level 2'), ('3','Level 3'), ('4', 'Level 4')], validators=[InputRequired()])
     DNA_sequence = TextAreaField('Paste your DNA sequence:', [InputRequired()])
     
-    # other options of input unable to successfully implement.
+#other options of form input unable to successfully implement.
     #default_genome = BooleanField('Use the default genome sequence:') 
     #upload_genome = FileField('Open DNA sequence to be encrypted:', [validators.InputRequired(), file_check])
     
-    
                                                   
-#form for decryption
+#form for decryption using wtform functions.
+#same theory as in the encryption form above.
 class DecryptionForm(Form):
     enter_key = PasswordField('Enter your secret key:', [InputRequired(), key_check])
     encrypted_DNA = TextAreaField('Paste the encrypted DNA sequence:', [InputRequired()])
-    
