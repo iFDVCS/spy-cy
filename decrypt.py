2be2d572a2350e08d4061776df6ff9b0f82ea0eb
AnnaDanese <anna.danese@etu.parisdescartes.fr>
2015-06-05 21:14:26 +0200

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -12,25 +12,58 @@ def mkstr(prefix):
 letters = ""
 ATGC_comb = mkstr(letters)
 
+# DJINA's function, used to find some specfics sequences into the genome where we incorporated
+# the message.
+def find_all(msg, site):
+    start = 0
+    while site in msg:
+        start = msg.find(site, start)
+        if start == -1: return
+        yield start
+        start += len(site) 
+
 # decryption function. It call other functions in order to decrypt the differents types of encryption. 
 def decrypt(key, genome_encrypted):
     
     # first we have a genome and we need to find where is our message.
     # At the end of the message we can find the restriction site that we will search to know where start and finish
     # our message.
-    genome_encrypted = list(genome_encrypted)
+    list_genome_encrypted = list(genome_encrypted)
+    list_genome_encrypted2 = []
+    message_encrypted = []
+    restrict_site = []
+    for i in range(8):
+        a = -(i+1)
+        restrict_site.append(a)
+    if restrict_site != 'AAAAAAAA':
+        position = find_all(restrict_site, genome_encrypted)
+        position = list(position)
+        if position != []:
+            first_site = position[0]
+            second_site = position[1]
+    # We found the two sites around our sequence, so we will keep the sequence between.
+            for i in range(second_site):
+                list_genome_encrypted2.append(i)
+            for i in range(first_site):
+                list_genome_encrypted2.pop(i)
+            message_encrypted = list_genome_encrypted2
+            
+    # if it is a sequence encrypted without genome:
+    else:
+        for i in range(8):
+            message_encrypted = list_genome_encrypted.pop(-1) 
 
-    encrypt_seq = []
-    DNA_letters = []
     
-    for chrct in genome_encrypted:
+    encrypt_seq = []
+    DNA_letters = []  
+    for chrct in message_encrypted:
         DNA_letters.append(chrct)
         if len(DNA_letters) ==4:
             DNA_letters = "".join(DNA_letters)
             encrypt_seq.append(DNA_letters)
             DNA_letters = []
 
-    if encrypt_seq[-1] == ATGC_comb[ord('1')]:
+    if DNA_letters == ATGC_comb[ord('1')]:
         import caesar_decrypt as cd
         return cd.caesar_decrypt(key, encrypted_seq)
     
