c110b4eefdfde1ee3b957ea94db66e6c841673d1
AnnaDanese <anna.danese@etu.parisdescartes.fr>
2015-06-05 17:25:32 +0200

    Update vigenere_encrypt.py

--- vigenere_encrypt.py
+++ vigenere_encrypt.py
@@ -1,3 +1,6 @@
+# vigenere cipher
+
+# it is combinatory list of ATGC words.
 def mkstr(prefix):
 		if len(prefix) == 4:
 			return [prefix]
@@ -9,20 +12,20 @@ def mkstr(prefix):
 		return combs
 
 letters = ''
-
 ATGC_comb = mkstr(letters)
 
 
-
+# function to encrypt message 
 def vigenere_encrypt(key, message):
     i = 0
     encrypt = []
     for chrct in message:
         i+= 1
-        letter = ATGC_comb[(int(ord(chrct)) + key[i])%256] # ERROR MESSAGE
+        letter = ATGC_comb[(int(ord(chrct)) + key[i])%256] # ERROR MESSAGE, I mix int and str and I haven't enough time
+ #								to fix that
         encrypt.append(letter)
 
-	# here I add the restriction site marker
+	# here I add the restriction site marker for the tncryption type 2
 	encrypt.append(ATGC_comb[ord('2')])
 	encrypt = ''.join(encrypt)
 
