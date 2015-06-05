2be7a7f02e3f7366db5fd4e8d47a12b15a7bdedd
AnnaDanese <anna.danese@etu.parisdescartes.fr>
2015-06-05 21:08:30 +0200

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -13,11 +13,16 @@ def find_all(msg, site):
 # right module to encrypt the message.
 
 def encrypt(message, key, level, genome):
+    
+    # I will do the assigment of the variable that we want return
+    encrypt_message = ''
+    
 
 # first cipher, it is working
     if level == 1:
         import caesar_encrypt as ce
         encrypt_message = ce.caesar_encrypt(key, message)
+
 
 # second cipher, it is not working, I am stuck at the end...
     elif level == 2:
@@ -50,16 +55,22 @@ def encrypt(message, key, level, genome):
 
 # before to encrypt the message we will found where in the genome we will put the message.
 # that's why we are searching a specific pattern. 
-
+    if genome != '':
 # fist we find a pattern of a specific sequence. DJINA did a dictionary to have choice but I haven't
 # the time to use it. The code is on github. I willuse just a sequence.
-    restrict_site = 'GATTTTAC'
-    position = find_all(genome, restrict_site)
-    genome = upper(genome)
-    genome_list = list(genome)
-    genome_list.insert(position, encrypt_message)
-    genome_list.insert((position + len(encrypt_message)), restrict_site)
-    genome_list.append(restrict_site)
-
-
-    return "".join(genome_list)
+        import string
+        restrict_site = 'GATTTTAC'
+        position = find_all(genome, restrict_site)
+        first_position = list(position)
+        first_position = position[0]
+        genome = str(genome)
+        genome = string.upper(genome)
+        genome_list = list(genome)
+        genome_list.insert(first_position, encrypt_message)
+        genome_list.insert((first_position + len(encrypt_message)), restrict_site)
+        genome_list.append(restrict_site)
+        return "".join(genome_list)
+    else:
+        encrypt_message = encrypt_message + "AAAAAAAA"
+        return encrypt_message
+        
