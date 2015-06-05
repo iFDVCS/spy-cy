76f7be38514f0e90e61f4293add5b044b7e16cfe
AnnaDanese <anna.danese@etu.parisdescartes.fr>
2015-06-05 14:02:52 +0200

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -21,39 +21,11 @@ def encrypt(key, message, genome, level):
 		encrypt_message = he.hill_encrypt(key, message)
 
 	elif level == 4:
-		import rossignol_encrypt as re
-		encrypt_message = re.rossignol_encrypt(key, message)
-
-	elif level == 5:
-		import linear_encrypt as le
-		encrypt_message = le.linear_encrypt(key, message)
+		import AES_encrypt as ae
+		encrypt_message = ae.AES_encrypt(key, message)
 
 	return encrypt_message
+    print encrypt_message
 
-
-	with open(genome) as genome_file:
-		genome_sequence = genome_file.read()
-
-
-	position = find_all(genome_sequence, ATTTTA)
-	print position
-
-# I haven't finish here (I am waiting for Djina's loop. Doesn't matter you can run the code
-	# ajoute encrypt_message position 63, à la fin du message on ajoute la sequence
-	# qu'on doit verifier et le niveau d'encryption.
-
-
-# input variable to test the code without connection with the interface	
-print " a key please"
-key = raw_input()
-
-print "position of your message"
-message = raw_input()
-
-print "position of your genome sequence"
-genome =raw_input()
-
-print"level of encryption"
-level = input()
-
-encrypt_message = encrypt(key, message, genome, level)
+#	position = find_all(genome_sequence, ATTTTA)
+#	print position
